c182cb249d9ed733c6ec3a890f137bb3c7d91c92
Milena <milenavasconcelos.info@gmail.com>
2025-07-09 10:49:55 -0400

    feat: Set up application routes

---
+++ src/pages/create-room.tsx
@@ -0,0 +1,8 @@
+export const CreateRoom = () => {
+  return (
+    <div>
+      <h1>Room Page</h1>
+      <p>This is the room page content.</p>
+    </div>
+  );
+};
---
+++ src/pages/room.tsx
@@ -0,0 +1,8 @@
+export const Room = () => {
+  return (
+    <div>
+      <h1>Room Page</h1>
+      <p>This is the room page content.</p>
+    </div>
+  );
+};
